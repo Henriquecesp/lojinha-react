d974068b3c6b254c8f26bfc1c217d7f0b07c6245
Henrique Cesp <48498237+Henriquecesp@users.noreply.github.com>
2019-11-20 17:49:23 -0300

    comentario aos

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
+// AOS Lib
 import App from './App';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 // Importando a nossa Lib de rotas
 import { BrowserRouter } from 'react-router-dom'
+//AOS Start
 AOS.init()
 
 ReactDOM.render((
