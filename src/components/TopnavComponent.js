fb35eb9a0dfa602c6cb67ac17e1d77e968fe1389
Henrique Cesp <48498237+Henriquecesp@users.noreply.github.com>
2019-11-23 16:09:41 -0300

    jQuery e css para mudar bg da topnav onScroll

--- src/components/TopnavComponent.js
+++ src/components/TopnavComponent.js
@@ -7,6 +7,25 @@
 
 import React from "react";
 import { NavLink } from 'react-router-dom'
+import $ from 'jquery'
+
+$(function($) {
+
+  var $nav = $('.topnav');
+  var $win = $(window);
+  var winH = $win.height();   // Get the window height.
+
+  $win.on("scroll", function () {
+      if ($(this).scrollTop() > winH ) {
+          $nav.addClass("nav-colored");
+      } else {
+          $nav.removeClass("nav-colored");
+      }
+  }).on("resize", function(){ // If the user resizes the window
+     winH = $(this).height(); // you'll need the new height value
+  });
+
+});
 
 const TopnavComponent = () => (
   <header className='topnav'>
