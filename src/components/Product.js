1566f8a12b6918aa7c295d01c4dbcca77cedc692
Henrique Cesp <48498237+Henriquecesp@users.noreply.github.com>
2019-11-25 01:35:48 -0300

    botao carrinho funcionando

--- src/components/Product.js
+++ src/components/Product.js
@@ -46,7 +46,7 @@ export default class Product extends Component {
                                                 -
                                                 </p>
                                                 ) : (
-                                                <span>Carrinho</span> //Carrinho
+                                                <span>Carrinho</span>
                                                 )}
                                     </button>
                                 </div>
