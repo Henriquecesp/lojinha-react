00d53ecf36775cf1961d6e1d1ccf8741e5ea7b8f
henriquecesp <henriquecesps4@gmail.com>
2019-11-26 20:23:46 -0300

    scss direto

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./sass/styles.css";
+import "./sass/styles.scss";
 import Main from "./Main";
 
 const App = () => (
