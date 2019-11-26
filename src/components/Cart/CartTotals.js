4d6f9013c372133c04952ac0ba86f000e8559916
Henrique Cesp <48498237+Henriquecesp@users.noreply.github.com>
2019-11-26 03:00:34 -0300

    button para continuar comprando e identacao

--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -1,23 +1,25 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { ButtonContinue } from "../ButtonscartComponent";
 
 const CartTotals = ({ value }) => {
-  const {cartTotal, clearCart } = value;
+  const { cartTotal, clearCart } = value;
   return (
     <React.Fragment>
       <div className='container'>
         <div className='row'>
-          <div className='col-10 mt-2 ml-sm-5 ml-md-auto col-sm-8 text-capitalize text-white'>
+          <div className='col-10 mt-2 ml-sm-5 ml-md-auto col-sm-8 text-capitalize text-white mb-3'>
             <NavLink to='/'>
               <button
-                className='btn btn-outline-danger text-uppercase mb-3 px-5'
+                className='btn btn-outline-danger text-uppercase m-1 px-5'
                 type='button'
                 onClick={() => clearCart()}
               >
                 Limpar Carrinho
               </button>
+              <ButtonContinue />
             </NavLink>
-            <h5>
+            <h5 className='pt-5'>
               <span className='text-title'>Total : </span>
               <strong>R$ {cartTotal}</strong>
             </h5>
