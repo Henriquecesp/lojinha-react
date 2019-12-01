0224a8bcdb2a318c0c736e3575600dd877da9c5f
Henrique Cesp <48498237+Henriquecesp@users.noreply.github.com>
2019-12-01 16:19:54 -0300

    att footer

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -9,20 +9,25 @@ export default class FooterComponent extends Component {
         <div className='row ml-5 pt-4 justify-content-md-center'>
           <div className='col-xs-1 col-lg-5 col-xl-6'>
             <p className='text-white text-uppercase text-footer'>CONTATO</p>
-            <p className='text-white'>
-              Lojinha uniamerica se encontra na sala 404.
-            </p>
+            <p className='text-white'>Contato@lojinhauniamerica.com.br
+           <br/> Av. das Cataratas, 1118 - Vila Yolanda, Foz do Iguaçu - PR, 85853-000</p>
           </div>
           <div className='col-xs-1 col-xl-3'>
             <p className='text-white text-uppercase'>menu</p>
             <NavLink to='/Products'>
-              <p><i>Produtos</i></p>
+              <p>
+                <i>Produtos</i>
+              </p>
             </NavLink>
             <NavLink to='/About'>
-              <p><i>Sobre</i></p>
+              <p>
+                <i>Sobre</i>
+              </p>
             </NavLink>
             <NavLink to='/Cart'>
-              <p><i>Carrinho</i></p>
+              <p>
+                <i>Carrinho</i>
+              </p>
             </NavLink>
           </div>
           <div className='col-xs-1 col-xl-3'>
@@ -44,7 +49,8 @@ export default class FooterComponent extends Component {
         <hr className='footer-line' />
         <div className='text-center text-white py-3'>
           <span>
-            Site feito por Henrique, Elison & Mauricio. Visite ES, sala 404
+            Site feito por Henrique, Elison & Mauricio. Visite Engenharia
+            Software, sala 404.
           </span>
         </div>
       </div>
