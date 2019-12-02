bf22300e93a97f764e0d2f7e2dcf9e11bd0a80d1
Henrique Cesp <48498237+Henriquecesp@users.noreply.github.com>
2019-12-02 02:32:12 -0300

    identacao

--- src/components/OpinionComponent.js
+++ src/components/OpinionComponent.js
@@ -3,20 +3,29 @@ import Title from "./Title";
 
 export default class OpinionComponent extends Component {
   render() {
-      let fontsize = {
-          fontSize: '2rem'
-      }
-      let user = {
-          name: 'joao da costa',
-          opinion: 'Site muito bom, nota 10 recomendo!'
-      }
+    let fontsize = {
+      fontSize: "2rem"
+    };
+    let user = {
+      name: "joao da costa",
+      opinion: "Site muito bom, nota 10 recomendo!"
+    };
     return (
-      <div className='container-fluid opnion my-5'>
+      <div
+        data-aos='fade-down'
+        data-aos-duration='2000'
+        className='container-fluid opinion my-5 pt-1'
+      >
         <Title name='usuarios' title='aprovam' />
         <div className='row text-center justify-content-center'>
-          <div className='col-10'>
-            <span className='text-center text-capitalize text-black' style={fontsize}>{user.name}</span>
-            <br/>
+          <div className='col-10 py-5'>
+            <span
+              className='text-center text-capitalize text-black'
+              style={fontsize}
+            >
+              {user.name}
+            </span>
+            <br />
             <span className='text-center'>"{user.opinion}"</span>
           </div>
         </div>
