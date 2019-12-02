def09111de4e03ca9d481dad0f69c9f3fa58c0af
Henrique Cesp <48498237+Henriquecesp@users.noreply.github.com>
2019-12-02 00:52:46 -0300

    input component created and add to footer

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
-import { InputGroup, FormControl, Button } from "react-bootstrap";
+import InputGroupComponent from "./InputGroupComponent";
 
 export default class FooterComponent extends Component {
   render() {
@@ -9,8 +9,11 @@ export default class FooterComponent extends Component {
         <div className='row ml-5 pt-4 justify-content-md-center'>
           <div className='col-xs-1 col-lg-5 col-xl-6'>
             <p className='text-white text-uppercase text-footer'>CONTATO</p>
-            <p className='text-white'>Contato@lojinhauniamerica.com.br
-           <br/> Av. das Cataratas, 1118 - Vila Yolanda, Foz do Iguaçu - PR, 85853-000</p>
+            <p className='text-white'>
+              Contato@lojinhauniamerica.com.br
+              <br /> Av. das Cataratas, 1118 - Vila Yolanda, Foz do Iguaçu - PR,
+              85853-000
+            </p>
           </div>
           <div className='col-xs-1 col-xl-3'>
             <p className='text-white text-uppercase'>menu</p>
@@ -34,16 +37,7 @@ export default class FooterComponent extends Component {
             <p className='text-white text-uppercase text-footer'>
               INSCREVA-SE para receber promocoes
             </p>
-            <InputGroup className='mb-3'>
-              <FormControl
-                placeholder='Seu email'
-                aria-label="Recipient's username"
-                aria-describedby='basic-addon2'
-              />
-              <InputGroup.Append>
-                <Button variant='success'>Inscrever</Button>
-              </InputGroup.Append>
-            </InputGroup>
+            <InputGroupComponent />
           </div>
         </div>
         <hr className='footer-line' />
