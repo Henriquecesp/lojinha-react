7056e41161565f104a09f9147c69f567fea980d3
Henrique Cesp <48498237+Henriquecesp@users.noreply.github.com>
2019-12-01 22:29:51 -0300

    featured att

--- src/components/Home/FeaturedComponent.js
+++ src/components/Home/FeaturedComponent.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 export default class FeaturedComponent extends Component {
   render() {
@@ -6,7 +8,7 @@ export default class FeaturedComponent extends Component {
       <div
         data-aos='fade-up'
         data-aos-duration='2000'
-        className='container-fluid featured'
+        className='container-fluid'
       >
         <div className='row mx-5 py-5 justify-content-md-center'>
           <div
@@ -14,11 +16,17 @@ export default class FeaturedComponent extends Component {
             data-aos-duration='3000'
             className='col-xs-1 col-xl-6'
           >
-              <img
-                src='./img/featured.jpg'
-                alt='featured img'
-                className='img-fluid'
-              />
+            <img
+              src='./img/featured.jpg'
+              alt='featured img'
+              className='img-fluid card'
+            />
+            <div className='inside-img'>
+              <p className='text-black text-uppercase'>
+                Conheça <FontAwesomeIcon icon={faArrowRight} />
+              </p>
+              <hr className='line' />
+            </div>
           </div>
 
           <div
@@ -26,17 +34,19 @@ export default class FeaturedComponent extends Component {
             data-aos-duration='3000'
             className='col-xs-1 col-xl-3'
           >
-            <div className='details'>
-              <div className='txt-1'>
-                <p className='text-white text-title'>Camisetas</p>
-                <hr className='footer-line' />
-                <p className='text-white'>Conforto & Estilo</p>
-              </div>
+            <div className='card details'>
               <img
                 src='./img/product-1.jpg'
-                alt='img camiseta'
-                className='img-fluid'
+                alt='camiseta'
+                className='card-img-top'
               />
+              <div className='card-body  featured'>
+                <p className='card-text text-center text-title text-white'>
+                  CAMISETAS
+                </p>
+                <hr className='footer-line' />
+                <p className='text-white text-center'>Conforto & Estilo</p>
+              </div>
             </div>
           </div>
 
@@ -45,17 +55,19 @@ export default class FeaturedComponent extends Component {
             data-aos-duration='3000'
             className='col-xs-1 col-xl-3'
           >
-            <div className='details'>
+            <div className='card details'>
+              <div className='card-body  featured'>
+                <p className='card-text text-center text-title text-white'>
+                  CANECAS
+                </p>
+                <hr className='footer-line' />
+                <p className='text-white text-center'>Personalidade</p>
+              </div>
               <img
                 src='./img/product-2.PNG'
-                alt='img caneca'
-                className='img-fluid'
+                alt='camiseta'
+                className='card-img-bottom'
               />
-              <div className='txt-1'>
-                <p className='text-white text-title'>Canecas</p>
-                <hr className='footer-line' />
-                <p className='text-white'>Personalidade</p>
-              </div>
             </div>
           </div>
         </div>
