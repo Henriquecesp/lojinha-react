011324210ae528a5defc08db8845c1f0c8881823
Henrique Cesp <48498237+Henriquecesp@users.noreply.github.com>
2019-12-02 00:53:11 -0300

    add the new promotion component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import TopnavComponent from '../components/TopnavComponent'
 import TextomeioComponent from '../components/Home/TextomeioComponent'
 import FeaturedComponent from "../components/Home/FeaturedComponent";
+import PromotionComponent from "../components/PromotionComponent";
 
 const Home = () => (
 <div>
     <TopnavComponent />
     <TextomeioComponent />
     <FeaturedComponent />
+    <PromotionComponent />
 </div>
 );
 
