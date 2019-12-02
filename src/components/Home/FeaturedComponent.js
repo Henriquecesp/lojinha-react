c423e11779361f148980f6bc782b6dd56ff36afa
Henrique Cesp <48498237+Henriquecesp@users.noreply.github.com>
2019-12-01 21:58:19 -0300

    att featured

--- src/components/Home/FeaturedComponent.js
+++ src/components/Home/FeaturedComponent.js
@@ -3,39 +3,55 @@ import React, { Component } from "react";
 export default class FeaturedComponent extends Component {
   render() {
     return (
-      <div className='container-fluid featured'>
+      <div
+        data-aos='fade-up'
+        data-aos-duration='2000'
+        className='container-fluid featured'
+      >
         <div className='row mx-5 py-5 justify-content-md-center'>
-          <div className='col-xs-1 col-xl-6'>
-            <img
-              src='./img/featured.jpg'
-              alt='featured img'
-              className='img-fluid img-2'
-            />
+          <div
+            data-aos='fade-right'
+            data-aos-duration='3000'
+            className='col-xs-1 col-xl-6'
+          >
+              <img
+                src='./img/featured.jpg'
+                alt='featured img'
+                className='img-fluid'
+              />
           </div>
 
-          <div className='col-xs-1 col-xl-3'>
+          <div
+            data-aos='fade-up'
+            data-aos-duration='3000'
+            className='col-xs-1 col-xl-3'
+          >
             <div className='details'>
-              <div className='txt-1 text-item'>
+              <div className='txt-1'>
                 <p className='text-white text-title'>Camisetas</p>
                 <hr className='footer-line' />
                 <p className='text-white'>Conforto & Estilo</p>
               </div>
               <img
                 src='./img/product-1.jpg'
-                alt='img product'
+                alt='img camiseta'
                 className='img-fluid'
               />
             </div>
           </div>
 
-          <div className='col-xs-1 col-xl-3'>
+          <div
+            data-aos='fade-left'
+            data-aos-duration='3000'
+            className='col-xs-1 col-xl-3'
+          >
             <div className='details'>
               <img
                 src='./img/product-2.PNG'
-                alt='img product'
+                alt='img caneca'
                 className='img-fluid'
               />
-              <div className='txt-1 text-item'>
+              <div className='txt-1'>
                 <p className='text-white text-title'>Canecas</p>
                 <hr className='footer-line' />
                 <p className='text-white'>Personalidade</p>
